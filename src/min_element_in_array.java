b5f08264b3405da62027d1df9e0790e2e1a86d97
Ashutosh2005-code <soashutosh@gmail.com>
2026-09-15 18:39:55 +0530

    Update minimum element in array

--- src/min_element_in_array.java
+++ src/min_element_in_array.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class min_element_in_array {
     static void main() {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = {
+                {1,2,3},
+                {4,5,6}
+        };
+        int min = arr[0][0];
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
+                if (arr[i][j] < min) {
+                    min = arr[i][j];
+                }
+            }
+            System.out.println(min);
+        }
+
+
     }
 
 }
